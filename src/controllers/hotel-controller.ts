d08fb134cc3fc9ad093d18642ecd3dd27aefabdc
davi-mancebo <davimancebo@gmail.com>
2023-04-23 23:36:36 -0300

    end of initial config and create of route

--- src/controllers/hotel-controller.ts
+++ src/controllers/hotel-controller.ts
@@ -1,10 +1,23 @@
 import { AuthenticatedRequest } from '@/middlewares';
 import { Response } from 'express';
+import httpStatus from 'http-status';
 
 export async function getAllHotel(req: AuthenticatedRequest, res: Response) {
   const { userId } = req;
+
+  try{
+
+  }catch(error){
+    return res.status(httpStatus.NOT_FOUND).send(error.message)
+  }
 }
 
 export async function getHotelById(req: AuthenticatedRequest, res: Response) {
   const { userId } = req;
+  
+  try{
+
+  }catch(error){
+    return res.status(httpStatus.NOT_FOUND).send(error.message)
+  }
 }
